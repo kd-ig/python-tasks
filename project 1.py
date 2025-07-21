b37725ca0569d01e2797be813842157d873ba005
kd-ig <kanaldeepsingh123@gmail.com>
2025-07-21 21:37:49 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,17 @@
+a=input('Enter 1st number:')
+b=input('Enter 2nd number:')
+
+a=float(a)
+b=float(b)
+
+add=a+b
+subs= a-b
+multi=a*b
+division=a/b
+
+print('Addition:',add)
+print('Subtraction:',subs)
+print('Multiplication:',multi)
+print('Division:',division)
+
+
---
+++ project 2.py
@@ -0,0 +1,8 @@
+a=input('Enter first name:')
+b=input('Enter last name:')
+
+a= str(a)
+b= str(b)
+
+c=b+'!'
+print('Hello,', a,c, 'Welcome to the Python program.')
